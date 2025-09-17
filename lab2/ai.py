00497316bbb23cfe9d7b821fabcd1aa4549e1055
fanwenlin <fireman.fwl@gmail.com>
2025-09-17 11:12:46 +0200

    draft: first implementation

--- lab2/ai.py
+++ lab2/ai.py
@@ -18,7 +18,37 @@ class Random(AI):
 class MinMax(AI):
     @staticmethod
     def best_move(current_state: State, objective: Objective):
-        pass
+        candidates_pits = current_state.available_moves()
+        if not candidates_pits:
+            return None
+        next_states = [current_state.next_state(pit) for pit in candidates_pits]
+        utilities = [current_state.score() for current_state in next_states]
+        decider = max if objective == Objective.MAX else min
+        return decider(candidates_pits, key=lambda pit: utilities[candidates_pits.index(pit)])
+    
+    @staticmethod
+    def get_utility(current_state: State, objective: Objective) -> float:
+        victory = current_state.check_victory()
+        if victory is not None:
+            # finished
+            if victory == -1:
+                # tie 
+                return 0
+            elif victory == 0:
+                # player 0 wins
+                return 1 if objective == Objective.MAX else -1
+            elif victory == 1:
+                # player 1 wins
+                return -1 if objective == Objective.MAX else 1
+    
+        # not finished, continue the game
+        candidate_moves = current_state.available_moves()
+        decider = max if objective == Objective.MAX else min
+        utilities = [MinMax.get_utility(current_state.next_state(move), objective) for move in candidate_moves]
+        return decider(utilities)
+
+
+
 
 
 class AlphaBeta(AI):
---
+++ lab2/note.md
@@ -0,0 +1,3 @@
+# Task1: 
+- [ ] implement recursive function which returns the utility of each available move
+- [ ] add counter for the number of expanded states, and output its value in the command line
